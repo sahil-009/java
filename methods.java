706ca88358eedb5b837f0ee23ec595d767b1aa90
Sahil Mund <130903382+sahil-009@users.noreply.github.com>
2023-12-15 10:25:34 +0530

    Add files via upload

---
+++ methods.java
@@ -0,0 +1,13 @@
+
+// methods :are block of clode that execute whenever it is called
+
+public class methods {
+    public static void main(String[] args) {
+        String name = "sahil";
+        hello(name);
+    }
+
+    static void hello(String title){
+        System.out.println("Hello " + title);
+    }
+}
